ac58d8ef335dec6d1720523951559f51a1e164d7
Woongsik <airwoong83@gmail.com>
2024-04-10 22:25:13 +0300

    Move Mailer keys to .env

--- src/config/email.ts
+++ src/config/email.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
+
 import { UserDocument } from '../model/UserModel';
+
 dotenv.config({ path: '.env' });
 
 export const sendWelcomeEmail = async (user: UserDocument, plainPasswordForGoogleLogin: string | null = null) => {  
@@ -8,11 +10,8 @@ export const sendWelcomeEmail = async (user: UserDocument, plainPasswordForGoogl
     apiKey: process.env.MAILER_API_KEY as string,
   });
   
-  const welcomeTemplateId: string = '3yxj6ljd931ldo2r';
-  const welcomeTemplateIdForGoogle: string = 'yzkq340je3xld796'; 
-  const templateId: string = plainPasswordForGoogleLogin ? welcomeTemplateIdForGoogle : welcomeTemplateId;
-  
-  const sender: string = 'admin@trial-jy7zpl93980l5vx6.mlsender.net';
+  const templateId: string = plainPasswordForGoogleLogin ? (process.env.MAILER_TEMPLATE_WELCOM as string) : (process.env.MAILER_TEMPLATE_WELCOME_FOR_GOOGLE_USER as string);
+  const sender: string = process.env.MAILER_SENDER as string;
   const sentFrom: Sender = new Sender(sender, "fs17-Node, Group 5");
   const recipients: Recipient[] = [
     new Recipient(user.email, `Hello, ${user.firstname}!`)
